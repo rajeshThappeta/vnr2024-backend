63164ee830734f8fb7e40c16222138867bd9ca00
rajesh <rajesh.t7982@gmail.com>
2024-10-21 20:25:39 +0530

    multiple apis demo

---
+++ APIs/productApi.js
@@ -0,0 +1,13 @@
+//crteate mini express app
+const exp=require('express')
+const productApp=exp.Router()
+
+//PRODUCT API routes
+//read all products
+//read a product by productID
+//create new product
+//update a product
+//delete a product by id
+
+
+module.exports=productApp;
--- server.js
+++ server.js
@@ -1,91 +1,17 @@
 //create HTTP server
-    const exp=require('express')
-    const app=exp()
-        //assign port number
-        app.listen(3000,()=>console.log('server on port 3000...'))
+const exp = require("express");
+const app = exp();
+//assign port number
+app.listen(3000, () => console.log("server on port 3000..."));
 
-        //add body parser middleware
-        app.use(exp.json())
-    
-    //create USER REST API(routes)
-       //test data(it will be replaced with DB later)
-       let users=[
-        {id:1,name:"kiran",age:21},
-        {id:2,name:"ravi",age:23}
-       ]
+//import user and product apps
+const userApp = require("./APIs/userApi");
+const productApp = require("./APIs/productApi");
 
-       //route to handle get users
-       app.get('/users',(req,res)=>{
-        //send all users
-        res.send({message:"all users",payload:users})
-       })
+//if path starts with user-api, then send req to userApi.js
+app.use("/user-api", userApp);
+
+//if path starts with product-api, then send req to productApi.js
+app.use("/product-api", productApp);
 
 
-       //route to handle get a user by id(URL param)
-       app.get('/users/:id',(req,res)=>{
-
-
-           // console.log(req.params) //{ id:  1 }
-            //get id from url (convert the string ID to number ID)
-           const paramId= Number(req.params.id) 
-           
-            //search user with this paramId
-           let result= users.find(user=>user.id===paramId)
-           //if user not found
-           if(result===undefined){
-            res.send({mesage:"User not found"})
-           }
-           //if user found
-           else{
-            res.send({message:"user",payload:result})
-           }
-           
-       })
-
-
-       //route to handle create user
-       app.post('/user',(req,res)=>{
-
-            //get user obj from req
-            let newUser=req.body;
-           //add newUser to users array
-           users.push(newUser)
-           //send
-           res.send({message:"New user created"})
-       })
-
-       //route to handle update user
-       app.put('/user',(req,res)=>{
-
-            //get modified user from client
-            let modifiedUser=req.body;
-            //find and replace user
-           let userIndex= users.findIndex(user=>user.id===modifiedUser.id)
-           //if user not found
-           if(userIndex===-1){
-            res.send({message:"User not found to update"})
-           }else{
-                users.splice(userIndex,1,modifiedUser)
-                res.send({message:"User modified"})
-           }
-
-       })
-
-       //route to handle delete a user by id
-       app.delete('/users/:id',(req,res)=>{
-
-            //get id from url param
-            let paramId=Number(req.params.id)
-            //find index of user matched with paramId
-            let userIndex= users.findIndex(user=>user.id===paramId)
-            //if user not found
-            if(userIndex===-1){
-                res.send({message:"User not found to delete"})
-            }else{
-                users.splice(userIndex,1)
-                res.send({message:"User removed"})
-            }
-
-       })
-
-       
