164d8fb914ae4067ed0b9fb45dd4793664f3a5c7
rajesh <rajesh.t7982@gmail.com>
2024-10-28 20:24:18 +0530

    user api with db

--- APIs/userApi.js
+++ APIs/userApi.js
@@ -2,79 +2,79 @@
 const exp = require("express");
 const userApp = exp.Router();
 
-
 //add body parser middleware
 userApp.use(exp.json());
 
 //create USER REST API(routes)
-//test data(it will be replaced with DB later)
-let users = [
-  { id: 1, name: "kiran", age: 21 },
-  { id: 2, name: "ravi", age: 23 },
-];
 
 //route to handle get users
-userApp.get("/users", (req, res) => {
-  //send all users
-  res.send({ message: "all users", payload: users });
+userApp.get("/users", async (req, res) => {
+  //get usersCollectionObj
+  const usersCollectionObj = req.app.get("usersCollectionObj");
+  //get users from db (find() returns db cursor, convert cursor to array using toArray())
+  const usersList = await usersCollectionObj.find().toArray();
+  //send res
+  res.send({ message: "users", payload: usersList });
 });
 
 //route to handle get a user by id(URL param)
-userApp.get("/users/:id", (req, res) => {
-  // console.log(req.params) //{ id:  1 }
-  //get id from url (convert the string ID to number ID)
-  const paramId = Number(req.params.id);
+userApp.get("/users/:id", async (req, res) => {
+  //get usersCollectionObj
+  const usersCollectionObj = req.app.get("usersCollectionObj");
 
-  //search user with this paramId
-  let result = users.find((user) => user.id === paramId);
-  //if user not found
-  if (result === undefined) {
-    res.send({ mesage: "User not found" });
-  }
-  //if user found
-  else {
-    res.send({ message: "user", payload: result });
-  }
+  //get id from url
+  const userId = Number(req.params.id);
+  //find user by id
+  const userObj = await usersCollectionObj.findOne({ id: userId });
+  //send res
+  res.send({ message: "user", payload: userObj });
 });
 
 //route to handle create user
-userApp.post("/user", (req, res) => {
-  //get user obj from req
-  let newUser = req.body;
-  //add newUser to users array
-  users.push(newUser);
-  //send
-  res.send({ message: "New user created" });
+userApp.post("/user", async (req, res) => {
+  //get usersCollectionObj
+  const usersCollectionObj = req.app.get("usersCollectionObj");
+  //get user obj from req.body
+  const newUser = req.body;
+  //insert into db
+  await usersCollectionObj.insertOne(newUser);
+  //send res
+  res.send({ message: "User created" });
 });
 
 //route to handle update user
-userApp.put("/user", (req, res) => {
-  //get modified user from client
-  let modifiedUser = req.body;
-  //find and replace user
-  let userIndex = users.findIndex((user) => user.id === modifiedUser.id);
-  //if user not found
-  if (userIndex === -1) {
-    res.send({ message: "User not found to update" });
+userApp.put("/user", async (req, res) => {
+  //get usersCollectionObj
+  const usersCollectionObj = req.app.get("usersCollectionObj");
+  //get modified user obj from req.body
+  const modifiedUser = req.body;
+  //update
+  let dbRes = await usersCollectionObj.updateOne(
+    { id: modifiedUser.id },
+    { $set: { ...modifiedUser } }
+  );
+  console.log(dbRes);
+  if (dbRes.modifiedCount === 1) {
+    res.send({ message: "User updated" });
   } else {
-    users.splice(userIndex, 1, modifiedUser);
-    res.send({ message: "User modified" });
+    res.send({ message: "User not modified" });
   }
 });
 
 //route to handle delete a user by id
-userApp.delete("/users/:id", (req, res) => {
-  //get id from url param
-  let paramId = Number(req.params.id);
-  //find index of user matched with paramId
-  let userIndex = users.findIndex((user) => user.id === paramId);
-  //if user not found
-  if (userIndex === -1) {
-    res.send({ message: "User not found to delete" });
+userApp.delete("/users/:id", async (req, res) => {
+  //get usersCollectionObj
+  const usersCollectionObj = req.app.get("usersCollectionObj");
+  //get id from url
+  const userId = Number(req.params.id);
+  //delete
+  let dbRes = await usersCollectionObj.deleteOne({ id: userId });
+  console.log(dbRes);
+  if (dbRes.deletedCount === 1) {
+    res.send({ message: "User deleted" });
   } else {
-    users.splice(userIndex, 1);
-    res.send({ message: "User removed" });
+    res.send({ message: "No user deleted" });
   }
 });
 
-module.exports=userApp;
+module.exports = userApp;
--- server.js
+++ server.js
@@ -1,8 +1,29 @@
 //create HTTP server
 const exp = require("express");
 const app = exp();
-//assign port number
-app.listen(3000, () => console.log("server on port 3000..."));
+
+//Connect to mongodb server
+const {MongoClient}=require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017')
+.then(mClient=>{
+    //get database object
+    const dbObj=mClient.db('testvnr')
+    //get users  & products collection objects 
+    const usersCollectionObj=dbObj.collection("users")
+    const productsCollectionObj=dbObj.collection("products")
+    //share usersCollectionObj to userApi
+    app.set('usersCollectionObj',usersCollectionObj)
+    app.set('productsCollectionObj',productsCollectionObj)
+    //assign port number
+    app.listen(3000, () => console.log("server on port 3000..."));
+    console.log("DB connection success")
+
+
+})
+.catch(err=>console.log("err in connecting db :",err))
+
+
 
 //import user and product apps
 const userApp = require("./APIs/userApi");
@@ -15,3 +36,8 @@ app.use("/user-api", userApp);
 app.use("/product-api", productApp);
 
 
+
+
+
+//db.users.insertOne()
+//usersCOllection.insertOne()
